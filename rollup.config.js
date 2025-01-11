fb1998650b6f10b06195d33f34eb17ce720f0bf7
Bruno Marotta <brunom@kopf.com.br>
2025-01-11 12:25:36 +0100

    Simplify build. Minify files

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json" assert { type: "json" };
+import terser from "@rollup/plugin-terser";
+import cleanup from "rollup-plugin-cleanup";
 
 export default [
     {
@@ -8,13 +10,32 @@ export default [
             {
                 file: "dist/color.js",
                 format: "cjs",
-                name: "colorwee"
+                name: "colorwee",
+                banner: ""
+            },
+            {
+                file: "dist/color.es.js",
+                format: "es",
+                name: "colorwee",
+                banner: ""
+            },
+            {
+                file: "dist/color.min.js",
+                format: "cjs",
+                name: "colorwee",
+                banner: "",
+                plugins: [terser()]
+            },
+            {
+                file: "dist/color.es.min.js",
+                format: "es",
+                name: "colorwee",
+                banner: "",
+                plugins: [terser()]
             }
         ],
         //preserveModules: true, // Uncomment to split in separate files
         external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-        plugins: [
-            typescript()
-        ]
+        plugins: [typescript(), cleanup({ comments: "none" })]
     }
 ];
